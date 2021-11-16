f7ae8b3a5f3b501c48368c693475275954b46280
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-11-17 00:52:05 +0100

    use useLocalStorage hook to control preferred theme within the app!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-import logo from "./logo.svg";
+import useLocalStorage from "use-local-storage";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  // set initial theme to dark
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  
+  // save preferred theme in local storage with key 'theme'
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <div className="App" data-theme={theme}>
+      <h1>Multi theme react app</h1>
+      <button onClick={switchTheme}>
+        Switch to {theme === "light" ? "Dark" : "Light"} Theme
+      </button>
+    </div>
+  );
 }
 
 export default App;
